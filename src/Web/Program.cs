f8bce5735c0f3249e95e54339dc3e59a0ada869f
Sergio Hernandez <sergio.hernandez.p@gmail.com>
2025-12-23 20:29:16 -0500

    Upgraded to NET 10

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -13,7 +13,6 @@
 //  limitations under the License.
 //
 
-using System.Reflection;
 using Ardalis.GuardClauses;
 using Microsoft.AspNetCore.HttpOverrides;
 using TrackHub.Security.Infrastructure.SecurityDB;
